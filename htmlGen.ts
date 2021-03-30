7e584eaf73a0509746f1222e4d2c8df825c395a4
noa <aggarwal.neeraj141@gmail.com>
2021-03-30 01:04:18 -0500

    Finish html generations

--- htmlGen.ts
+++ htmlGen.ts
@@ -1,17 +1,59 @@
-const fs = require("fs");
+import * as util from "./utils/generators";
 
-const writeToFile = async (path, data) => {
-  let filehandle = null;
+const name = "neeraj";
+const subtitle = "subtitle goes here";
+const section1 = "experiences";
+const exp = "experience 1";
+const time = "june 1";
+const desc = "description";
+const copyright = "copyright";
+const footer_link = "footer link";
 
-  try {
-    filehandle = await fs.promises.open(path, "w");
-    await filehandle.writeFile(data);
-  } finally {
-    if (filehandle) {
-      await filehandle.close();
-    }
-  }
-};
+let htmlString: string = `
+<!DOCTYPE html>
+<html lang="en">
 
-writeToFile("hi.txt", "lol");
-// div`inner text goes here`;
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">`;
+
+htmlString += util.genTag`title${name}`;
+htmlString += util.endTag`head`;
+
+htmlString += util.startTag`body`;
+htmlString += util.startTag`header`;
+htmlString += util.genTag`h1${name}`;
+htmlString += util.genTag`p${subtitle}`;
+htmlString += util.endTag`header`;
+
+htmlString += util.startTag`section`;
+htmlString += util.genTag`h2${section1}`;
+
+htmlString += util.startTag`ul`;
+
+for (let i: number = 0; i < 3; i += 1) {
+  htmlString += util.startTag`li`;
+  htmlString += util.genTag`h3${exp}`;
+  htmlString += util.genTag`h4${time}`;
+  htmlString += util.genTag`p${desc}`;
+  htmlString += util.endTag`li`;
+}
+
+htmlString += util.endTag`ul`;
+
+htmlString += util.startTag`footer`;
+htmlString += util.genTag`p${copyright}`;
+htmlString += util.startTag`ul`;
+for (let i: number = 0; i < 2; i += 1) {
+  htmlString += util.startTag`li`;
+  htmlString += util.genTag`p${footer_link}`;
+  htmlString += util.endTag`li`;
+}
+htmlString += util.endTag`ul`;
+htmlString += util.endTag`footer`;
+
+htmlString += util.endTag`body`;
+htmlString += util.endTag`html`;
+
+util.writeToFile("lol.html", htmlString);
--- utils/generators.ts
+++ utils/generators.ts
@@ -1,5 +1,17 @@
-function div(text) {
-  return `<div>${text}</div>`;
-}
+const fs = require("fs");
 
-div`inner text goes here`;
+export const genTag = (tags: TemplateStringsArray, text: string) =>
+  `<${tags[0]}>${text}</${tags[0]}>`;
+
+export const startTag = (tag: TemplateStringsArray) => `<${tag}>`;
+export const endTag = (tag: TemplateStringsArray) => `</${tag}>`;
+
+export const writeToFile = async (path: string, data: string) => {
+  let filehandle = null;
+  try {
+    filehandle = await fs.promises.open(path, "w");
+    await filehandle.writeFile(data);
+  } finally {
+    if (filehandle) await filehandle.close();
+  }
+};
