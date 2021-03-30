61335bde3a87cbc990f562d957de7047431f5762
noa <aggarwal.neeraj141@gmail.com>
2021-03-30 01:22:16 -0500

    Add support for attributes

--- utils/generators.ts
+++ utils/generators.ts
@@ -2,21 +2,28 @@ const fs = require("fs");
 
 /**
  * Will generate both the start and end tag, as well as putting
- * the text in between.
- * @param tags – the type of tag to be put.
+ * the text in between. If an attribute for the tag is provided, it
+ * is included as well.
+ * @param tags – an array of the strings given. The first value is the
+ *               tag to be used, and the second is the attribute.
  * @param text – the text to be put in between the tags.
  * @returns the full tag and text to be added to the html string.
  */
 export const genTag = (tags: TemplateStringsArray, text: string) =>
-  `<${tags[0]}>${text}</${tags[0]}>`;
+  tags.length > 1
+    ? `<${tags[0]} class="${tags[1]}">${text}</${tags[0]}>`
+    : `<${tags[0]}>${text}</${tags[0]}>`;
 
 /**
  * Will generate only a single tag. To make it an ending tag, the \
- * should be appended to the provided string.
- * @param tag – the type of tag to be put.
+ * should be appended to the provided string. If an attribute for the
+ * tag is provided, it is included as well.
+ * @param tags – an array of the strings given. The first value is the
+ *               tag to be used, and the second is the attribute.
  * @returns the full tag to be added to the html string.
  */
-export const genSingleTag = (tag: TemplateStringsArray) => `<${tag}>`;
+export const genSingleTag = (tags: TemplateStringsArray) =>
+  tags.length > 1 ? `<${tags[0]} class="${tags[1]}">` : `<${tags[0]}>`;
 
 /**
  * Will write the given data to the provided file.
