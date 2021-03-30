5e1016c032da3469751ba1a1e3118f8c184a4d53
noa <aggarwal.neeraj141@gmail.com>
2021-03-30 01:27:42 -0500

    Add compiled js

--- htmlGen.js
+++ htmlGen.js
@@ -1,67 +1,48 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+"use strict";
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var _this = this;
-var fs = require("fs");
-var writeToFile = function (path, data) { return __awaiter(_this, void 0, void 0, function () {
-    var filehandle;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                filehandle = null;
-                _a.label = 1;
-            case 1:
-                _a.trys.push([1, , 4, 7]);
-                return [4 /*yield*/, fs.promises.open(path, "w")];
-            case 2:
-                filehandle = _a.sent();
-                return [4 /*yield*/, filehandle.writeFile(data)];
-            case 3:
-                _a.sent();
-                return [3 /*break*/, 7];
-            case 4:
-                if (!filehandle) return [3 /*break*/, 6];
-                return [4 /*yield*/, filehandle.close()];
-            case 5:
-                _a.sent();
-                _a.label = 6;
-            case 6: return [7 /*endfinally*/];
-            case 7: return [2 /*return*/];
-        }
-    });
-}); };
-writeToFile("hi.txt", "lol");
-// div`inner text goes here`;
+exports.__esModule = true;
+var util = require("./utils/generators");
+var name = "neeraj";
+var subtitle = "subtitle goes here";
+var section1 = "experiences";
+var exp = "experience 1";
+var time = "june 1";
+var desc = "description";
+var copyright = "copyright";
+var footer_link = "footer link";
+var htmlString = "\n<!DOCTYPE html>\n<html lang=\"en\">\n\n<head>\n  <meta charset=\"UTF-8\">\n  <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
+htmlString += util.genTag(templateObject_1 || (templateObject_1 = __makeTemplateObject(["title", ""], ["title", ""])), name);
+htmlString += util.genSingleTag(templateObject_2 || (templateObject_2 = __makeTemplateObject(["/head"], ["/head"])));
+htmlString += util.genSingleTag(templateObject_3 || (templateObject_3 = __makeTemplateObject(["body"], ["body"])));
+htmlString += util.genSingleTag(templateObject_4 || (templateObject_4 = __makeTemplateObject(["header"], ["header"])));
+htmlString += util.genTag(templateObject_5 || (templateObject_5 = __makeTemplateObject(["h1", ""], ["h1", ""])), name);
+htmlString += util.genTag(templateObject_6 || (templateObject_6 = __makeTemplateObject(["p", ""], ["p", ""])), subtitle);
+htmlString += util.genSingleTag(templateObject_7 || (templateObject_7 = __makeTemplateObject(["/header"], ["/header"])));
+htmlString += util.genSingleTag(templateObject_8 || (templateObject_8 = __makeTemplateObject(["section"], ["section"])));
+htmlString += util.genTag(templateObject_9 || (templateObject_9 = __makeTemplateObject(["h2", ""], ["h2", ""])), section1);
+htmlString += util.genSingleTag(templateObject_10 || (templateObject_10 = __makeTemplateObject(["ul"], ["ul"])));
+for (var i = 0; i < 3; i += 1) {
+    htmlString += util.genSingleTag(templateObject_11 || (templateObject_11 = __makeTemplateObject(["li"], ["li"])));
+    htmlString += util.genTag(templateObject_12 || (templateObject_12 = __makeTemplateObject(["h3", ""], ["h3", ""])), exp);
+    htmlString += util.genTag(templateObject_13 || (templateObject_13 = __makeTemplateObject(["h4", ""], ["h4", ""])), time);
+    htmlString += util.genTag(templateObject_14 || (templateObject_14 = __makeTemplateObject(["p", ""], ["p", ""])), desc);
+    htmlString += util.genSingleTag(templateObject_15 || (templateObject_15 = __makeTemplateObject(["/li"], ["/li"])));
+}
+htmlString += util.genSingleTag(templateObject_16 || (templateObject_16 = __makeTemplateObject(["/ul"], ["/ul"])));
+htmlString += util.genSingleTag(templateObject_17 || (templateObject_17 = __makeTemplateObject(["footer"], ["footer"])));
+htmlString += util.genTag(templateObject_18 || (templateObject_18 = __makeTemplateObject(["p", ""], ["p", ""])), copyright);
+htmlString += util.genSingleTag(templateObject_19 || (templateObject_19 = __makeTemplateObject(["ul"], ["ul"])));
+for (var i = 0; i < 2; i += 1) {
+    htmlString += util.genSingleTag(templateObject_20 || (templateObject_20 = __makeTemplateObject(["li"], ["li"])));
+    htmlString += util.genTag(templateObject_21 || (templateObject_21 = __makeTemplateObject(["p", ""], ["p", ""])), footer_link);
+    htmlString += util.genSingleTag(templateObject_22 || (templateObject_22 = __makeTemplateObject(["/li"], ["/li"])));
+}
+htmlString += util.genSingleTag(templateObject_23 || (templateObject_23 = __makeTemplateObject(["/ul"], ["/ul"])));
+htmlString += util.genSingleTag(templateObject_24 || (templateObject_24 = __makeTemplateObject(["/footer"], ["/footer"])));
+htmlString += util.genSingleTag(templateObject_25 || (templateObject_25 = __makeTemplateObject(["/body"], ["/body"])));
+htmlString += util.genSingleTag(templateObject_26 || (templateObject_26 = __makeTemplateObject(["/html"], ["/html"])));
+util.writeToFile("lol.html", htmlString);
+var templateObject_1, templateObject_2, templateObject_3, templateObject_4, templateObject_5, templateObject_6, templateObject_7, templateObject_8, templateObject_9, templateObject_10, templateObject_11, templateObject_12, templateObject_13, templateObject_14, templateObject_15, templateObject_16, templateObject_17, templateObject_18, templateObject_19, templateObject_20, templateObject_21, templateObject_22, templateObject_23, templateObject_24, templateObject_25, templateObject_26;
