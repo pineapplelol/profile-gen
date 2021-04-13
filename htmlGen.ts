6b941c061d9f9b5b1ec3771e1e76e5e0a000b0af
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2021-04-13 05:32:34 +0000

    🌵 Fix bugs, add documentation (#4)

--- htmlGen.ts
+++ htmlGen.ts
@@ -8,7 +8,13 @@ let htmlString: string = `
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">`;
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <style>
+    :root {
+      --primary: ${data.theme.color}};
+    }
+  </style>`;
 
 htmlString += util.genTag("title", data.name);
 htmlString += util.genSingleTag("/head");
